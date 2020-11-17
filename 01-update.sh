ec75d0ef14fd2ccdd771ad7be6e60baf7cfdf830
Jonathan Santos <57300906+jtn-san@users.noreply.github.com>
2020-11-17 20:19:17 -0300

    Add files via upload

--- 01-update.sh
+++ 01-update.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 echo -e "Atualizando as  lista d Apt"
+    apt update
+
+echo -e "Atualizando os software"
+    apt -y upgrade
+
+echo -e "Forçando atualização do sistema"
+    apt -y full-upgrade
+
+echo -e "Forçando atualização de segurança"
+    apt -y dist-upgrade 
+
+echo -e "Removendo software desnecessários"
+    apt -y autoremove
+    apt -y autoclean
+    apt clean
+
+echo -e "Sistema atualizado, pressione <Enter> para o reboot"
+read
+sleep 10
+reboot
