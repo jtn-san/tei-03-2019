010cfccf6682474942653945755a92b323a42762
Jonathan Santos <57300906+jtn-san@users.noreply.github.com>
2020-11-19 18:59:17 -0300

    Obs
    Com observação na instalação de sua versão.

--- 03-phpmyadmin.sh
+++ 03-phpmyadmin.sh
@@ -5,7 +5,8 @@ echo -e "Atualizando as Listas do Apt"
     apt update
 
 echo -e "Instalando o PHPMyAdmin no Ubuntu Server"
-    #obs: na versão >= 8.0 do MySQL não utilizamos mais o usuário root
+    #obs: na versão >= 8.0 do MySQL 
+    #não utilizamos mais o usuário root
     #para se logar no PhpMyAdmin, sendo necessário criar um novo usuário
     #sudo mysql -u root -p
     #CREATE USER 'jtn'@'localhost' IDENTIFIED WITH mysql_native_password BY 'jtn';
