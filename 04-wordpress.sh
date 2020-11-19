c9e51c44444f1115788744aee5bc8e856f1affcb
Jonathan Santos <57300906+jtn-san@users.noreply.github.com>
2020-11-19 19:28:57 -0300

    Add files via upload

---
+++ 04-wordpress.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+clear
+
+#Criando a base de dados no MySQL
+# sudo mysql -u root -p
+# CREATE DATABASE wordpress;
+# CREATE USER 'wordpress' IDENTIFIED BY 'wordpress';
+# GRANT USAGE ON *.* TO 'wordpress' IDENTIFIED BY 'wordpress';
+# GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress';
+# FLUSH PRIVILEGES;
+# exit
+#
+#Testando o acesso ao banco de dados com o usuário Wordpress
+# sudo mysql -u wordpress -p
+# SHOW DATABASES;
+# exit
+
+
+echo -e "Atualizando as Listas do Apt"
+    apt update
+
+echo -e "Instalando o Gunzip no Ubuntu Server"
+    apt -y install Gunzip
+
+echo -e "Download o Wordpress no Ubuntu Server"
+    wget https://wordpress.org/latest.zip
+
+echo -e "Descompactando o Wordpress"
+    gunzip latest.zip
+
+echo -e "Acessando o diretório do Wordpress"
+    cd wordpress
+
+echo -e "Criando o arquivo de configuração do Wordpress"
+    cp wp-config-sample.php wp-config.php
+
